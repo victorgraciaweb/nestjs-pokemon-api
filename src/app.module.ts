80505d0941d26736f490a28270b8f0e89c127faa
Víctor Gracia Magallón <info@victorgraciaweb.com>
2024-03-30 02:34:51 +0200

    Added seed module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,12 @@ import { join } from 'path';
 import { PokemonModule } from './pokemon/pokemon.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { CommonModule } from './common/common.module';
+import { SeedModule } from './seed/seed.module';
 
 @Module({
   imports: [
     ServeStaticModule.forRoot({
-      rootPath: join(__dirname,'..','public'), 
+      rootPath: join(__dirname, '..', 'public'),
     }),
 
     MongooseModule.forRoot('mongodb://localhost:27017/nest-pokemon'),
@@ -16,6 +17,8 @@ import { CommonModule } from './common/common.module';
     PokemonModule,
 
     CommonModule,
+
+    SeedModule,
   ],
 })
-export class AppModule {}
+export class AppModule { }
